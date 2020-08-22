1843c8c4330f3421fbd7382283bc85b7a3d9efe6
Mark Kozel <development@markkozel.net>
2020-08-22 09:19:28 -0700

    Modified tests per src code changes

--- tests/workingFolder.js
+++ tests/workingFolder.js
@@ -1,7 +1,8 @@
 const WorkingContainer = require("../src/WorkingContainer");
-const fileUtils = require("../src/utils/fileUtils");
+const getProjRoot = require("../src/utils/fileUtils").getProjRoot;
 
 const path = require('path');
+const git = require('../src/utils/gitUtils');
 
 let repoCont1 = new WorkingContainer(process.cwd(), true);
 console.log(repoCont1.isReady());
@@ -10,7 +11,7 @@ console.log(repoCont1.getCurrentBranch())
 console.log(repoCont1.getLog())
 
 
-let repoCont2 = new WorkingContainer(path.join(fileUtils.getProjRoot(), "tests", "zz_TestingRepos", "working1"), false);
+let repoCont2 = new WorkingContainer(path.join(getProjRoot(), "tests", "zz_TestingRepos", "working1"), false);
 if (repoCont2.isReady()) {
   console.log(repoCont2.getCurrentBranch())
   console.log(repoCont2.checkoutBranch('br1'));
@@ -18,7 +19,7 @@ if (repoCont2.isReady()) {
   console.log(repoCont2.getLog())
 }
 
-let repoCont3 = new WorkingContainer(path.join(fileUtils.getProjRoot(), "tests", "zz_TestingRepos", "working1"), true);
+let repoCont3 = new WorkingContainer(path.join(getProjRoot(), "tests", "zz_TestingRepos", "working1"), true);
 if (repoCont3.isReady()) {
   console.log(repoCont3.getCurrentBranch())
   console.log(repoCont3.checkoutBranch('br1'));
@@ -26,3 +27,8 @@ if (repoCont3.isReady()) {
   console.log(repoCont3.getStatus())
   console.log(repoCont3.getLog())
 }
+
+gitSingleton = new git();
+console.log(`Last Command: ${gitSingleton.lastCmd}`);
+console.log(`Last Result: ${gitSingleton.lastResult}`);
+console.log(`Last Error: ${gitSingleton.lastError}`);
